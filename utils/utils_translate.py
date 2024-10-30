3bd43223ae8d72245a8ab625b7f8fd4abdb7717b
yurimarcon <yuri.erik.oliveira@gmail.com>
2024-10-30 12:39:00 -0300

    Fix bug when send same language in source and destin

--- utils/utils_translate.py
+++ utils/utils_translate.py
@@ -4,10 +4,12 @@ from utils.utils_loger import log_error
 translator = Translator()
 
 def translate_text (text, source_lang, dest_language):
+
+    src_lang = "en" if source_lang == dest_language : source_lang
     try:
         translated = translator.translate(
             text=text, 
-            src=source_lang,
+            src=src_lang,
             dest=dest_language
             )
         return translated.text.replace(".", "")
